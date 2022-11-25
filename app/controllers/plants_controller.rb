a231d68a60cfef9ed6cf8ec4f75f5de2a3b2c0ba
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-26 01:32:06 +0300

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,7 +9,11 @@ class PlantsController < ApplicationController
   # GET /plants/:id
   def show
     plant = Plant.find_by(id: params[:id])
-    render json: plant
+    if plant
+      render json: plant
+    else
+      render_not_found_response
+    end
   end
 
   # POST /plants
@@ -18,9 +22,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH  - update plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+  # DELETE /birds/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
 end
